330ed2f3ec5ada3cce23334826640e2521e312e0
spandandb <spandandb94@gmail.com>
2025-01-08 11:58:40 +0530

    Open modal window functionality implemented

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const crossModal = document.querySelector(".close-modal");
+const showModal = document.querySelectorAll(".show-modal");
+const overlay = document.querySelector(".overlay");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener("click", openModal);
+}
